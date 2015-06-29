b1418584a071d722b7f47d362c7eba3887253260
stoplime <slanimero@gmail.com>
2015-06-28 23:32:40 -0400

    Added Unity version

---
+++ IARCsim/Assets/Scripts/GRMotion.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRMotion : MonoBehaviour {
+
+	public float Speed;
+	public float AngularSpeed;
+
+	private float initialRot;
+	public float InitialRot{
+		get{return initialRot;}
+		set{initialRot = value;}
+	}
+
+	private float rot;//final rotation without delay (radians)
+	private float currentRot;//visual rotation (radians)
+	private float time;
+	private bool play;
+
+	private float counter5;
+	private float counter20;
+
+	// Use this for initialization
+	void Start () {
+		play = false;
+		time = 0;
+		rot = initialRot;
+		currentRot = initialRot;
+
+		counter5 = 0;
+		counter20 = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			play = !play;
+		}
+		if (play) {
+			float delta = Time.deltaTime;
+			time += delta;
+			counter5 += delta;
+			counter20 += delta;
+
+			if (counter20 >= 20) {
+				counter20 = 0;
+				rot += Mathf.PI + RotateNoise (3);
+			}
+			if (counter5 >= 5) {
+				counter5 = 0;
+				rot += RotateNoise (20) + RotateNoise (3);
+			}
+			//adjusting rotation
+			float deltaRot = rot - currentRot;
+			Debug.Log(rot);
+			Vector3 angularVel = new Vector3 (0, AngularSpeed, 0) * Time.deltaTime;
+			if (deltaRot != 0) {
+				angularVel *= deltaRot / Mathf.Abs (deltaRot);
+			}
+			if (angularVel.magnitude > deltaRot) {
+				angularVel *= 0;
+			}
+			this.transform.Rotate (-angularVel*180/Mathf.PI);
+			currentRot += angularVel.magnitude;
+
+			//adjusting Velocity
+			//if (Mathf.Abs(deltaRot) < 5*Mathf.PI/180) {
+				Vector3 vel = new Vector3 (Mathf.Cos (currentRot), 0, Mathf.Sin (currentRot)) * Speed * Time.deltaTime;
+				
+				this.transform.localPosition += vel;
+			//}
+		}
+	}
+
+	public float RotateNoise (float angle) {
+		return ((Random.value * 2 * angle) - angle) * Mathf.PI / 180;
+	}
+}
---
+++ IARCsim/Assets/Scripts/GenerateField.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateField : MonoBehaviour {
+
+	public GameObject FloorTile;
+	public Vector2 TileSize;
+
+	public GameObject R_GroundRobot;
+	public GameObject G_GroundRobot;
+
+	private Vector2 topLeft;
+
+	// Use this for initialization
+	void Start () {
+		topLeft = TileSize * (-9.5f);
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				CreateTile(FloorTile, new Vector2 (topLeft.x+(i*TileSize.x), topLeft.y+(j*TileSize.y)), TileSize);
+			}
+		}
+		int numbOfRobots = 10;
+		int numbOfRed = numbOfRobots / 2;
+		float radius = 1;
+		for (int i = 0; i < numbOfRobots; i++) {
+			float angle = i*Mathf.PI*2/numbOfRobots;
+			Vector2 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle))*radius;
+
+			if (numbOfRed > 0 && i+numbOfRed<10 ) {
+				if (Random.value >= 0.5f) {
+					numbOfRed--;
+					CreateRobot(R_GroundRobot, pos, TileSize, angle, "Red Robot");
+				}else{
+					CreateRobot(G_GroundRobot, pos, TileSize, angle, "Green Robot");
+				}
+			}else if(numbOfRed > 0){
+				numbOfRed--;
+				CreateRobot(R_GroundRobot, pos, TileSize, angle, "Red Robot");
+			}else{
+				CreateRobot(G_GroundRobot, pos, TileSize, angle, "Green Robot");
+			}
+		}
+
+	}
+
+	public void CreateTile (GameObject preFabFloor, Vector2 pos, Vector2 size)
+	{
+		GameObject f = (GameObject)Instantiate (preFabFloor, new Vector3 (pos.x, 0, pos.y), Quaternion.identity);
+		f.transform.parent = transform;
+		f.transform.localScale = new Vector3 (size.x, 1, size.y) / 10f;
+		f.gameObject.name = "Grid Tile";
+	}
+
+	public void CreateRobot (GameObject robot, Vector2 pos, Vector2 size, float angle, string name)
+	{
+		GameObject r = (GameObject)Instantiate (robot, new Vector3 (pos.x, 0.046f, pos.y), Quaternion.identity);
+		r.transform.parent = transform;
+		r.transform.Rotate (new Vector3 (0, -angle*180/Mathf.PI, 0));
+		r.transform.localScale.Scale (new Vector3(size.x, 1, size.y));
+		GRMotion m = r.GetComponent<GRMotion> ();
+		m.InitialRot = angle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
